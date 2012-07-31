12ce0d6446be274e665238bbe7b299dc97507268
brycecr <brycecr@gmail.com>
2012-07-31 13:39:35 -0700

    fixing

--- MSMExplorer/make.sh
+++ MSMExplorer/make.sh
@@ -3,7 +3,6 @@
 cd /Users/gestalt/Documents/msmexplorer_git/msmexplorer/MSMExplorer/
 ant
 if [ $? == 0 ]; then
-	cp ./dist/MSMExplorer.jar ./MSMExplorer.jar
-	java -enableassertions -jar ./MSMExplorer.jar
+	java -enableassertions -jar ./dist/MSMExplorer.jar
 fi
 
--- MSMExplorer/run.sh
+++ MSMExplorer/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar ./MSMExplorer.jar
+java -jar ./dist/MSMExplorer.jar
