3124acf90e05b1d73d5324130278c1c67947bd36
brycecr <brycecr@gmail.com>
2012-08-02 14:33:17 -0700

    forgot one...

---
+++ MSMExplorer/src/edu/stanford/folding/msmexplorer/MSMExplorerLauncher.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.stanford.folding.msmexplorer;
+
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author gestalt
+ */
+public class MSMExplorerLauncher {
+	
+	public static void main (String[] argv) {
+		if (System.getProperty("os.name").startsWith("Mac")) {
+			try {
+				// Set System L&F
+				System.setProperty("apple.laf.useScreenMenuBar", "true");
+				System.setProperty("com.apple.mrj.application.apple.menu.about.name", "MSMExplorer");
+				UIManager.setLookAndFeel(
+					UIManager.getSystemLookAndFeelClassName());
+			}
+			catch (Exception e) {
+				// handle exception
+			}
+		}
+		new MSMExplorer();
+	}
+
+}
