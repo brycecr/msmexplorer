9630238cba12d70ef2260dc35ec53a801be7770f
brycecr <brycecr@gmail.com>
2012-08-03 10:52:43 -0700

    fixed imports

--- MSMExplorer/src/edu/stanford/folding/msmexplorer/util/axis/AxisSettingsDialog.java
+++ MSMExplorer/src/edu/stanford/folding/msmexplorer/util/axis/AxisSettingsDialog.java
@@ -4,7 +4,6 @@ import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
